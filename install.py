c19593a60f33e8a25f184b1a313e8094b6e43f84
Artur <77436065+artur-asimov@users.noreply.github.com>
2022-01-28 19:29:43 +0200

    Add files via upload

--- install.py
+++ install.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 import os
+import os.path
+import shutil
 os.system('pip install psutil py-cpuinfo datetime colorama && sudo cp tinyfetch /usr/bin/ && sudo chmod +x tinyfetch')
+
+if not os.path.isdir(f'/home/{os.getlogin()}/.config/tinyfetch/') :
+    os.mkdir(f'/home/{os.getlogin()}/.config/tinyfetch/')
+
+shutil.copyfile(f'./config.ini', f'/home/{os.getlogin()}/.config/tinyfetch/config.ini')
